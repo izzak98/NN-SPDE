c2a61decf3edc36c235c6d5ab19b2e116cf81d12
izzak98 <izzak98@hotmail.com>
2024-11-18 22:38:59 +0000

    Refactor code for improved flexibility and clarity in trial parameter generation

--- utils/optuna_utils.py
+++ utils/optuna_utils.py
@@ -1,7 +1,7 @@
 def gen_dgm_params(trial):
     n_hidden = trial.suggest_int("n_hidden", 0, 5)
     hidden_dims = [trial.suggest_categorical(
-        f"n_params_{i}", [4, 8, 16, 32, 64, 128, 256, 512]) for i in range(n_hidden)]
+        f"n_params_{i}", [4, 8, 16, 32, 64, 128, 256]) for i in range(n_hidden)]
     dgm_dims = trial.suggest_categorical("dgm_dims", [0, 4, 8, 16, 32, 64, 128, 256])
     params = {
         "hidden_dims": hidden_dims,
@@ -16,10 +16,10 @@ def gen_dgm_params(trial):
 def gen_mim_params(trial):
     n_u_hidden = trial.suggest_int("n_u_hidden", 1, 5)
     u_hidden_dims = [trial.suggest_categorical(
-        f"n_u_params_{i}", [4, 8, 16, 32, 64, 128, 256, 512]) for i in range(n_u_hidden)]
+        f"n_u_params_{i}", [4, 8, 16, 32, 64, 128, 256]) for i in range(n_u_hidden)]
     n_p_hidden = trial.suggest_int("n_p_hidden", 1, 5)
     p_hidden_dims = [trial.suggest_categorical(
-        f"n_p_params_{i}", [4, 8, 16, 32, 64, 128, 256, 512]) for i in range(n_p_hidden)]
+        f"n_p_params_{i}", [4, 8, 16, 32, 64, 128, 256]) for i in range(n_p_hidden)]
     u_dgm_dims = trial.suggest_categorical("u_dgm_dims", [0, 4, 8, 16, 32, 64, 128, 256])
     p_dgm_dims = trial.suggest_categorical("p_dgm_dims", [0, 4, 8, 16, 32, 64, 128, 256])
     params = {
