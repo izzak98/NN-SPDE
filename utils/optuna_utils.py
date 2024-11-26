adca9e0e8666626ba283364db18eabbbd347ff9a
izzak98 <izzak98@hotmail.com>
2024-11-26 21:19:53 +0000

    Update output activation options to restrict choices for improved model performance

--- utils/optuna_utils.py
+++ utils/optuna_utils.py
@@ -8,7 +8,7 @@ def gen_dgm_params(trial):
         "dgm_dims": dgm_dims,
         "n_dgm_layers": trial.suggest_int("n_dgm_layers", 1, 5) if dgm_dims > 0 else 0,
         "hidden_activation": trial.suggest_categorical("hidden_activation", ["relu", "tanh", "sigmoid"]),
-        "output_activation": trial.suggest_categorical("output_activation", ["relu", "tanh", "sigmoid", ""])
+        "output_activation": trial.suggest_categorical("output_activation", ["tanh", ""])
     }
     return params
 
@@ -28,7 +28,7 @@ def gen_mim_params(trial):
         "u_activation": trial.suggest_categorical("u_activation", ["relu", "tanh", "sigmoid"]),
         "u_out_activation": trial.suggest_categorical("u_output_activation", ["relu", "tanh", "sigmoid", ""]),
         "p_activation": trial.suggest_categorical("p_activation", ["relu", "tanh", "sigmoid"]),
-        "p_out_activation": trial.suggest_categorical("p_output_activation", ["relu", "tanh", "sigmoid", ""]),
+        "p_out_activation": trial.suggest_categorical("p_output_activation", ["tanh", ""]),
         "u_dgm_dims": u_dgm_dims,
         "u_n_dgm_layers": trial.suggest_int("u_n_dgm_layers", 1, 5) if u_dgm_dims > 0 else 0,
         "p_dgm_dims": p_dgm_dims,
