fb707258558e0b025d4f5685eec0d1f4d70cd86a
izzak98 <izzak98@hotmail.com>
2024-12-27 01:40:19 +0000

    Add Optuna tuning functionality for DGM and MIM models; implement hyperparameter optimization and training logic.

---
+++ utils/optuna_utils.py
@@ -0,0 +1,27 @@
+def get_model_hyper_params(trial, sub_model_name):
+    n_fc_layers = trial.suggest_int(f"{sub_model_name}_n_fc_layers", 1, 5)
+    hidden_dims = []
+    for i in range(n_fc_layers):
+        hidden_dims.append(trial.suggest_categorical(
+            f"{sub_model_name}_fc_layer_{i}_dim", [32, 64, 128, 256, 512]))
+    use_dgm = trial.suggest_categorical(f"{sub_model_name}_use_dgm", [1, 0])
+    use_dgm = bool(use_dgm)
+    if use_dgm:
+        dgm_dims = trial.suggest_categorical(
+            f"{sub_model_name}_dgm_dims", [2, 4, 8, 16, 32, 64, 128])
+        n_dgm_layers = trial.suggest_int(f"{sub_model_name}_n_dgm_layers", 1, 5)
+    else:
+        dgm_dims = 0
+        n_dgm_layers = 0
+    output_activation = trial.suggest_categorical(
+        f"{sub_model_name}_output_activation", ["relu", "tanh", "sigmoid", None])
+    hidden_activation = trial.suggest_categorical(
+        f"{sub_model_name}_hidden_activation", ["relu", "tanh", "sigmoid"])
+    model_params = {
+        "hidden_dims": hidden_dims,
+        "dgm_dims": dgm_dims,
+        "n_dgm_layers": n_dgm_layers,
+        "output_activation": output_activation,
+        "hidden_activation": hidden_activation
+    }
+    return model_params
