757ea4eabd1cd2543f1ecffc6fbe38bcc6714da0
izzak98 <izzak98@hotmail.com>
2025-02-02 23:03:20 +0000

    Add function to extract best hyperparameters from Optuna trials for sub-models

--- utils/optuna_utils.py
+++ utils/optuna_utils.py
@@ -25,3 +25,28 @@ def get_model_hyper_params(trial, sub_model_name):
         "hidden_activation": hidden_activation
     }
     return model_params
+
+
+def get_best_model_hyper_params(best_trial, sub_model_name):
+    """Extracts best hyperparameters for the given sub-model from an Optuna best trial."""
+    n_fc_layers = best_trial.params[f"{sub_model_name}_n_fc_layers"]
+    hidden_dims = [
+        best_trial.params[f"{sub_model_name}_fc_layer_{i}_dim"] for i in range(n_fc_layers)
+    ]
+
+    use_dgm = bool(best_trial.params[f"{sub_model_name}_use_dgm"])
+    if use_dgm:
+        dgm_dims = best_trial.params[f"{sub_model_name}_dgm_dims"]
+        n_dgm_layers = best_trial.params[f"{sub_model_name}_n_dgm_layers"]
+    else:
+        dgm_dims = 0
+        n_dgm_layers = 0
+
+    model_params = {
+        "hidden_dims": hidden_dims,
+        "dgm_dims": dgm_dims,
+        "n_dgm_layers": n_dgm_layers,
+        "output_activation": best_trial.params[f"{sub_model_name}_output_activation"],
+        "hidden_activation": best_trial.params[f"{sub_model_name}_hidden_activation"],
+    }
+    return model_params
